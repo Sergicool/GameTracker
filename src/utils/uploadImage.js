3ea1eb70e9498e04738bccbe3b1cd6c97353816e
Sergicool <Sergioal200222@gmail.com>
2025-05-21 21:45:30 +0200

    Cleanup

--- src/utils/uploadImage.js
+++
@@ -1,9 +0,0 @@
-import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
-import { storage } from "../firebase"; // TODO inicialización
-
-export const uploadImage = async (file) => {
-  const fileRef = ref(storage, `games/${file.name}`);
-  await uploadBytes(fileRef, file);
-  const url = await getDownloadURL(fileRef);
-  return url;
-};
